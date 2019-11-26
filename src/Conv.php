cc7785a8ed6a3663ea3bf2121f5329dcfbc6b4ad
Massimiliano Arione <garakkio@gmail.com>
2019-11-26 16:28:34 +0100

    step 2

--- src/Conv.php
+++ src/Conv.php
@@ -4,8 +4,30 @@ namespace Kata;
 
 class Conv
 {
+    static private $conv = [
+        10 => 'A',
+        11 => 'B',
+        12 => 'C',
+        13 => 'D',
+        14 => 'E',
+        15 => 'F',
+    ];
+
     public function convert(int $dec): string
     {
-        return 'TODO...';
+        if ($dec < 10) {
+            return $dec;
+        }
+        $pezzi = [];
+        while (true) {
+            $resto = $dec % 16;
+            $pezzi[] = $resto < 10 ? $resto : self::$conv[$resto];
+            $dec = (int) floor($dec / 16);
+            if ($dec <= 0) {
+                break;
+            }
+        }
+
+        return implode('', \array_reverse($pezzi));
     }
 }
